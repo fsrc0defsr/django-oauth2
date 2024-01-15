1b4d8a1565a1cf3378e6ecfbbf78cc37aa8c75c7
fsrc0defsr <fsrcodefsr@gmail.com>
2024-01-16 02:19:00 +0600

    commit "Authorization_grant_type = Client Credentials"

--- iam/utils.py
+++ iam/utils.py
@@ -10,3 +10,9 @@ code_challenge = base64.urlsafe_b64encode(code_challenge).decode('utf-8').replac
 
 print(code_verifier)
 print(code_challenge)
+
+
+client_id="web_id"
+secret="web_secret"
+credential = "{0}:{1}".format(client_id, secret)
+print(base64.b64encode(credential.encode("utf-8")))
